1f3994b489ccc805c289a7d72a1887b3d99b48aa
Arun Bharath <arunbharath91@gmail.com>
2020-04-27 14:44:00 +0530

    update

--- src/app/decorators.ts
+++ src/app/decorators.ts
@@ -2,3 +2,54 @@ export const Freeze = (constructor: Function) => {
   Object.freeze(constructor);
   Object.freeze(constructor.prototype);
 }
+
+export const Debounce = (wait: number, immediate: boolean = false) => {
+
+  return (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
+
+    // store timeout value for cancel the timeout
+    let timeoutID: any;
+    // store original function for future use
+    const original = descriptor.value;
+    descriptor.value = function(args: any) {
+      const lazyFn = () => {
+        timeoutID = false;
+        if (!immediate) original.apply(this, args);
+      }
+      const callNow = (immediate && !timeoutID)
+      if (timeoutID) clearTimeout(timeoutID);
+      timeoutID = setTimeout(lazyFn, wait);
+      if (callNow) original.apply(this, args);
+    }
+    return descriptor;
+  }
+}
+
+
+export const Throttle = (wait: number) => {
+
+  return (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
+
+    // store timeout value for cancel the timeout
+    let lastFn: any;
+    let lastRan: any;
+    // store original function for future use
+    const original = descriptor.value;
+    descriptor.value = function(args: any) {
+      if (!lastRan) {
+        original.apply(this, args)
+        lastRan = Date.now()
+      } else {
+        clearTimeout(lastFn)
+        lastFn = setTimeout(() => {
+          if ((Date.now() - lastRan) >= wait) {
+            original.apply(this, args)
+            lastRan = Date.now()
+          }
+        }, wait - (Date.now() - lastRan))
+      }
+
+    }
+    return descriptor;
+  }
+}
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,30 @@
-import { Freeze } from "./decorators";
+import { Freeze, Debounce, Throttle } from "./decorators";
 
 @Freeze
 export class FreezedClass {
 
 }
+
+class Http {
+  constructor() {
+
+  }
+
+  @Debounce(2000)
+  fetchApi() {
+  console.log('hi')
+  }
+
+  @Throttle(2000)
+  fetchApi2() {
+  console.log('hi')
+  }
+}
+const http = new Http();
+(document.querySelector('#button') as HTMLElement).addEventListener('click', () => {
+  http.fetchApi();
+});
+
+(document.querySelector('#button2') as HTMLElement).addEventListener('click', () => {
+  http.fetchApi2();
+});
